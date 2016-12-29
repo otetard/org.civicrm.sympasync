16e0ccac6596155ca81d47a2ec455bea5859421c
Olivier Tétard <olivier.tetard@miskin.fr>
2016-12-29 17:45:38 +0100

    Rename `staticGroups` to `groups` in AngularJS code.

--- ang/sympasync/AdminCtrl.js
+++ ang/sympasync/AdminCtrl.js
@@ -5,11 +5,10 @@
         controller: 'SympasyncAdminCtrl',
         templateUrl: '~/sympasync/AdminCtrl.html',
         resolve: {
-          staticGroups: function(crmApi) {
+          groups: function(crmApi) {
             return crmApi('Group', 'get', {
               'option.limit': 0,
               is_active: 1,
-              saved_search_id: {'IS NULL': 1},
               return: 'title'
             });
           },
@@ -21,14 +20,14 @@
     }
   );
 
-  angular.module('sympasync').controller('SympasyncAdminCtrl', function($scope, crmApi, crmStatus, crmUiHelp, sqlCred, staticGroups) {
+  angular.module('sympasync').controller('SympasyncAdminCtrl', function($scope, crmApi, crmStatus, crmUiHelp, sqlCred, groups) {
     var ts = $scope.ts = CRM.ts('sympasync');
     var hs = $scope.hs = crmUiHelp({file: 'CRM/sympasync/AdminCtrl'}); // See: templates/CRM/sympasync/AdminCtrl.hlp
 
     $scope.sqlCred = sqlCred.values;
     $scope.sqlView = 'sympa_subscribers';
     $scope.selected = {groupId: null};
-    $scope.staticGroups = staticGroups.values;
+    $scope.groups = groups.values;
     $scope.unsubscribeBaseUrl = CRM.url('civicrm/sympa/unsubscribe');
     if ($scope.unsubscribeBaseUrl[0] === '/') {
       $scope.unsubscribeBaseUrl = window.location.protocol + '//' + window.location.host + $scope.unsubscribeBaseUrl;
